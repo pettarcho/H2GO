ab3e59c02e86c50be45f7b6dfb663c156c94cdd0
Kaloyan <kokomarinov11@gmail.com>
2026-06-12 11:39:42 +0200

    Fixed backend

--- backend/H2GO.API/obj/Debug/net10.0/H2GO.API.AssemblyInfo.cs
+++ backend/H2GO.API/obj/Debug/net10.0/H2GO.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("H2GO.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e964b9e95aa81919696ce3e03eb4c49a94617f29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a7542cc5961ae129384c114e518998d8533cc11")]
 [assembly: System.Reflection.AssemblyProductAttribute("H2GO.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("H2GO.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
